b17274ab3a2524d8777f9c4afb88ff05c30ac73c
Douglas Gardner <douglas@chippy.ch>
2022-01-27 17:56:42 +0000

    Fix linting issues

--- config.js
+++ config.js
@@ -2,49 +2,49 @@ const convict = require('convict')
 
 // These should all be sensible production defaults
 // For the defaults when in development mode check config/development.json
-const config = module.exports = convict({
-    env: {
-        doc: 'The application environment',
-        format: ['production', 'development', 'test'],
-        default: 'production',
-        env: 'NODE_ENV'
+module.exports = convict({
+  env: {
+    doc: 'The application environment',
+    format: ['production', 'development', 'test'],
+    default: 'production',
+    env: 'NODE_ENV'
+  },
+  server: {
+    port: {
+      doc: 'The port to bind',
+      format: 'port',
+      default: 3000,
+      env: 'PORT',
+      arg: 'port'
     },
-    server: {
-        port: {
-            doc: 'The port to bind',
-            format: 'port',
-            default: 3000,
-            env: 'PORT',
-            arg: 'port'
-        },
-        host: {
-            doc: 'The IP to bind',
-            default: 'localhost',
-            env: 'HOST',
-            arg: 'host'
-        }
-    },
-    templating: {
-        watch: {
-            doc: 'Whether to watch templates for changes',
-            format: Boolean,
-            default: false,
-            arg: 'watch'
-        },
-        cache: {
-            doc: 'Whether to cache templates',
-            format: Boolean,
-            default: false,
-            args: 'cache'
-        }
-    },
-    endpoint: {
-        info: {
-            name: {
-                doc: 'URL of the info-service',
-                format: URL,
-                env: 'INFO_SERVICE'
-            }
-        }
+    host: {
+      doc: 'The IP to bind',
+      default: 'localhost',
+      env: 'HOST',
+      arg: 'host'
     }
+  },
+  templating: {
+    watch: {
+      doc: 'Whether to watch templates for changes',
+      format: Boolean,
+      default: false,
+      arg: 'watch'
+    },
+    cache: {
+      doc: 'Whether to cache templates',
+      format: Boolean,
+      default: false,
+      args: 'cache'
+    }
+  },
+  endpoint: {
+    info: {
+      name: {
+        doc: 'URL of the info-service',
+        format: URL,
+        env: 'INFO_SERVICE'
+      }
+    }
+  }
 })
